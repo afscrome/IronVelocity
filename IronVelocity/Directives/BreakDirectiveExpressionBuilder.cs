2db44b48b57ed64c2c46662b67dd0d44bf8dc883
Alex Crome <afscrome@users.noreply.github.com>
2014-01-12 16:22:33 +0000

    Missing files from previous commit

---
+++ IronVelocity/Directives/BreakDirectiveExpressionBuilder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace IronVelocity.Directives
+{
+    public class BreakDirectiveExpressionBuilder : DirectiveExpressionBuilder
+    {
+        public override Expression Build(NVelocity.Runtime.Parser.Node.ASTDirective node, VelocityASTConverter converter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ IronVelocity/Scope.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace IronVelocity
+{
+    public interface IScope
+    {
+        Expression GetVariable(string name);
+    }
+
+    public class BaseScope : IScope
+    {
+        private static readonly PropertyInfo _indexerProperty = typeof(VelocityContext).GetProperty("Item", typeof(Expression), new[] { typeof(string) });
+
+        private readonly Expression _context;
+
+        public BaseScope(Expression context)
+        {
+            if (context == null)
+                throw new ArgumentNullException("context");
+
+            if (context.Type != typeof(VelocityContext))
+                throw new ArgumentOutOfRangeException("context");
+
+            _context = context;
+        }
+
+        public Expression GetVariable(string name)
+        {
+            return Expression.MakeIndex(_context, _indexerProperty, new[] { Expression.Constant(name) });
+        }
+
+    }
+
+}
