4cebf7efe5d4a06f722b00bfc17989f61edf6e6c
Alex Crome <afscrome@users.noreply.github.com>
2015-05-16 12:25:06 +0100

    Expanded on some of the implicit conversions tests in preperation for refactoring.

--- IronVelocity.Tests/Binders/ReflectionHelper/ImplicitConversionSupportTests.cs
+++ IronVelocity.Tests/Binders/ReflectionHelper/ImplicitConversionSupportTests.cs
@@ -1,27 +1,18 @@
 ﻿using IronVelocity.Reflection;
 using NUnit.Framework;
 using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace IronVelocity.Tests.Binders
 {
     /// <summary>
-    /// This class performs tests against ReflectionHelper.IsArgumentCompatible to ensure
-    /// 
+    /// This class performs tests against ReflectionHelper.IsArgumentCompatible
     /// </summary>
     public class ImplicitConversionSupportTests
     {
         private readonly IArgumentConverter _conversionHelper = new ArgumentConverter();
-        //TODO: 6.1.3??  - Implicit enumeration conversions
-        //TODO: 6.1.4??  - Implicit nullable conversions
-        //TODO: 6.1.6??  - Implicit reference conversions
         //TODO: 6.1.11?? - User-defined implicit conversions
-        // 6.1.8  - Not applicable as we're binding at runtime
-        // 6.1.10 - Not applicable as we're not supporting generics
-
-        // 6.1.12 - Not applicable as we're not supporting anonymous types
-
-
-
 
         //6.1.1 - Identity Conversions
         [TestCase(typeof(sbyte), TestName = "Identity Conversion: Primitive")]
@@ -35,7 +26,12 @@ namespace IronVelocity.Tests.Binders
             Assert.IsTrue(result);
         }
 
+        //6.1.2 - Implicit numeric conversions - At end of file.
+
         //6.1.3 - Enumeration conversions
+        //WRONG - not how the C# spec states.  However should we work this way in Velocity since
+        //VTL doesn't have a concept of enums?  Better to support conversion of string to enum?
+        /*
         [TestCase(typeof(EnvironmentVariableTarget), typeof(int), true)]
         [TestCase(typeof(int), typeof(EnvironmentVariableTarget), false)]
         [TestCase(typeof(EnvironmentVariableTarget), typeof(long), true)]
@@ -45,43 +41,90 @@ namespace IronVelocity.Tests.Binders
             var result = _conversionHelper.CanBeConverted(from, to);
             Assert.AreEqual(expected, result);
         }
+        */
 
-        //6.2.1 - Implicit Numeric conversions
-
-        //6.1.5
-        [Test]
-        public void NullLiteralConversion_SucceedesWithReferenceType()
+        //6.1.4 Implicit nullable conversions
+        [TestCase(typeof(int?), typeof(long?), true, Ignore = true)]
+        [TestCase(typeof(long?), typeof(int?), false)]
+        [TestCase(typeof(int), typeof(long?), true, Ignore = true)]
+        [TestCase(typeof(long), typeof(int?), false)]
+        public void ImplicitNullableConversions(Type from, Type to, bool expected)
         {
-            var result = _conversionHelper.CanBeConverted(null, typeof(Child));
-            Assert.IsTrue(result);
+            var result = _conversionHelper.CanBeConverted(from, to);
+            Assert.That(result, Is.EqualTo(expected));
         }
 
-        [Test]
-        public void NullLiteralConversion_FailsWithPrimitive()
+        //6.1.5 Null literal conversions
+        [TestCase(typeof(Child), true)]
+        [TestCase(typeof(int), false)]
+        [TestCase(typeof(Guid), false)]
+        public void NullLiteralConversion(Type type, bool expected)
         {
-            var result = _conversionHelper.CanBeConverted(null, typeof(int));
-            Assert.IsFalse(result);
+            var result = _conversionHelper.CanBeConverted(null, type);
+            Assert.That(result, Is.EqualTo(expected));
         }
 
-        [Test]
-        public void NullLiteralConversion_FailsWithValueType()
+        //6.1.6 Implicit reference conversions
+        //Reference to object
+        [TestCase(typeof(Child), typeof(object), true)]
+        // Class S to Class T
+        [TestCase(typeof(object), typeof(Child), false)]
+        [TestCase(typeof(Child), typeof(Parent), true)]
+        [TestCase(typeof(Parent), typeof(Child), false)]
+        // Class S to Interface T
+        [TestCase(typeof(List<string>), typeof(IList), true)]
+        [TestCase(typeof(ICollection), typeof(List<string>), false)]
+        //Interface S to Interface T
+        [TestCase(typeof(IList<string>), typeof(ICollection<string>), true)]
+        [TestCase(typeof(ICollection<string>), typeof(IList<string>), false)]
+        [TestCase(typeof(IReadOnlyList<string>), typeof(IReadOnlyCollection<object>), true)]
+        [TestCase(typeof(IReadOnlyCollection<string>), typeof(IReadOnlyList<object>), false)]
+        //S[] to T[]
+        [TestCase(typeof(string[]), typeof(object[]), true)]
+        [TestCase(typeof(int[]), typeof(object[]), false)]
+        [TestCase(typeof(object[]), typeof(Child[]), false)]
+        [TestCase(typeof(Child[,]), typeof(object[,]), true)]
+        [TestCase(typeof(object[,]), typeof(string[,]), false)]
+        [TestCase(typeof(string[]), typeof(string[,]), false)]
+        [TestCase(typeof(int[]), typeof(long[]), false)]
+        [TestCase(typeof(int[]), typeof(object[]), false)]
+        // S[] to Array
+        [TestCase(typeof(string[]), typeof(Array), true)]
+        [TestCase(typeof(string[]), typeof(IStructuralComparable), true)]
+        // S[] to IList<T>
+        [TestCase(typeof(string[]), typeof(IList<string>), true)]
+        [TestCase(typeof(string[]), typeof(ICollection), true)]
+        [TestCase(typeof(string[,]), typeof(IList<string>), false)]
+        public void ImplicitReferenceConversions(Type from, Type to, bool expected)
         {
-            var result = _conversionHelper.CanBeConverted(null, typeof(Guid));
-            Assert.IsFalse(result);
+            var result = _conversionHelper.CanBeConverted(from, to);
+            Assert.That(result, Is.EqualTo(expected));
         }
 
-        //6.1.7
+        //6.1.7 Boxing Conversions
         [Test]
         public void BoxingConversion()
         {
             var result = _conversionHelper.CanBeConverted(typeof(int), typeof(object));
             Assert.IsTrue(result);
-
         }
 
+        //6.1.8 Implicit dynamic conversions -N/A
+        //6.1.9 Implicit constant expression conversions - N/A
 
+        //6.1.10 Implicit conversions involving type paramaters
+        [TestCase(typeof(IReadOnlyList<IReadOnlyList<string>>), typeof(IReadOnlyList<IReadOnlyCollection<string>>), true)]
+        [TestCase(typeof(IReadOnlyList<IReadOnlyCollection<string>>), typeof(IReadOnlyList<IReadOnlyList<string>>), false)]
+        [TestCase(typeof(IReadOnlyList<List<string>>), typeof(IReadOnlyList<IReadOnlyCollection<string>>), true)]
+        [TestCase(typeof(IReadOnlyList<IReadOnlyCollection<string>>), typeof(List<IReadOnlyList<string>>), false)]
+        public void ImplicitConversionInvolvingTypeParameters(Type from, Type to, bool expected)
+        {
+            var result = _conversionHelper.CanBeConverted(from, to);
+            Assert.That(result, Is.EqualTo(expected));
+        }
 
-        //Signed Byte
+        //6.1.2 - Implicit Numeric conversions
+        //SByte
         [TestCase(typeof(sbyte),   typeof(byte),    false, TestName = "Primitive Implicit Conversion: sbyte to byte")]
         [TestCase(typeof(sbyte),   typeof(short),   true,  TestName = "Primitive Implicit Conversion: sbyte to short")]
         [TestCase(typeof(sbyte),   typeof(ushort),  false, TestName = "Primitive Implicit Conversion: sbyte to ushort")]
@@ -104,9 +147,8 @@ namespace IronVelocity.Tests.Binders
         [TestCase(typeof(byte),    typeof(char),    false, TestName = "Primitive Implicit Conversion: byte to char")]
         [TestCase(typeof(byte),    typeof(float),   true,  TestName = "Primitive Implicit Conversion: byte to float")]
         [TestCase(typeof(byte),    typeof(double),  true,  TestName = "Primitive Implicit Conversion: byte to double")]
-        [TestCase(typeof(byte),    typeof(decimal), true, TestName = "Primitive Implicit Conversion: byte to decimal")]
-
-        //16-bit Integer
+        [TestCase(typeof(byte),    typeof(decimal), true,  TestName = "Primitive Implicit Conversion: byte to decimal")]
+        //Int16
         [TestCase(typeof(short),   typeof(sbyte),   false, TestName = "Primitive Implicit Conversion: short to sbyte")]
         [TestCase(typeof(short),   typeof(byte),    false, TestName = "Primitive Implicit Conversion: short to byte")]
         [TestCase(typeof(short),   typeof(ushort),  false, TestName = "Primitive Implicit Conversion: short to ushort")]
@@ -118,7 +160,7 @@ namespace IronVelocity.Tests.Binders
         [TestCase(typeof(short),   typeof(float),   true,  TestName = "Primitive Implicit Conversion: short to float")]
         [TestCase(typeof(short),   typeof(double),  true,  TestName = "Primitive Implicit Conversion: short to double")]
         [TestCase(typeof(short),   typeof(decimal), true,  TestName = "Primitive Implicit Conversion: short to decimal")]
-        //16-bit Unsigned Integer        
+        //UInt16       
         [TestCase(typeof(ushort),  typeof(sbyte),   false, TestName = "Primitive Implicit Conversion: ushort to sbyte")]
         [TestCase(typeof(ushort),  typeof(byte),    false, TestName = "Primitive Implicit Conversion: ushort to byte")]
         [TestCase(typeof(ushort),  typeof(short),   false, TestName = "Primitive Implicit Conversion: ushort to short")]
@@ -130,7 +172,7 @@ namespace IronVelocity.Tests.Binders
         [TestCase(typeof(ushort),  typeof(float),   true,  TestName = "Primitive Implicit Conversion: ushort to float")]
         [TestCase(typeof(ushort),  typeof(double),  true,  TestName = "Primitive Implicit Conversion: ushort to double")]
         [TestCase(typeof(ushort),  typeof(decimal), true,  TestName = "Primitive Implicit Conversion: ushort to decimal")]
-        //32-bit Integer        
+        //Int32   
         [TestCase(typeof(int),     typeof(sbyte),   false, TestName = "Primitive Implicit Conversion: int to sbyte")]
         [TestCase(typeof(int),     typeof(byte),    false, TestName = "Primitive Implicit Conversion: int to byte")]
         [TestCase(typeof(int),     typeof(short),   false, TestName = "Primitive Implicit Conversion: int to short")]
@@ -142,8 +184,8 @@ namespace IronVelocity.Tests.Binders
         [TestCase(typeof(int),     typeof(float),   true,  TestName = "Primitive Implicit Conversion: int to float")]
         [TestCase(typeof(int),     typeof(double),  true,  TestName = "Primitive Implicit Conversion: int to double")]
         [TestCase(typeof(int),     typeof(decimal), true,  TestName = "Primitive Implicit Conversion: int to decimal")]
-        //32-bit Unsigned Integer        
-        [TestCase(typeof(uint),    typeof(sbyte), false, TestName = "Primitive Implicit Conversion: uint to sbyte")]
+        //UInt32        
+        [TestCase(typeof(uint),    typeof(sbyte),   false, TestName = "Primitive Implicit Conversion: uint to sbyte")]
         [TestCase(typeof(uint),    typeof(byte),    false, TestName = "Primitive Implicit Conversion: uint to byte")]
         [TestCase(typeof(uint),    typeof(short),   false, TestName = "Primitive Implicit Conversion: uint to short")]
         [TestCase(typeof(uint),    typeof(ushort),  false, TestName = "Primitive Implicit Conversion: uint to ushort")]
@@ -154,7 +196,7 @@ namespace IronVelocity.Tests.Binders
         [TestCase(typeof(uint),    typeof(float),   true,  TestName = "Primitive Implicit Conversion: uint to float")]
         [TestCase(typeof(uint),    typeof(double),  true,  TestName = "Primitive Implicit Conversion: uint to double")]
         [TestCase(typeof(uint),    typeof(decimal), true,  TestName = "Primitive Implicit Conversion: uint to decimal")]
-        //64-bit Integer                
+        //Int64               
         [TestCase(typeof(long),    typeof(sbyte),   false, TestName = "Primitive Implicit Conversion: long to sbyte")]
         [TestCase(typeof(long),    typeof(byte),    false, TestName = "Primitive Implicit Conversion: long to byte")]
         [TestCase(typeof(long),    typeof(short),   false, TestName = "Primitive Implicit Conversion: long to short")]
@@ -166,7 +208,7 @@ namespace IronVelocity.Tests.Binders
         [TestCase(typeof(long),    typeof(float),   true,  TestName = "Primitive Implicit Conversion: long to float")]
         [TestCase(typeof(long),    typeof(double),  true,  TestName = "Primitive Implicit Conversion: long to double")]
         [TestCase(typeof(long),    typeof(decimal), true,  TestName = "Primitive Implicit Conversion: long to decimal")]
-        //64-bit Unsigned Integer
+        //UInt64
         [TestCase(typeof(ulong),   typeof(sbyte),   false, TestName = "Primitive Implicit Conversion: ulong to sbyte")]
         [TestCase(typeof(ulong),   typeof(byte),    false, TestName = "Primitive Implicit Conversion: ulong to byte")]
         [TestCase(typeof(ulong),   typeof(short),   false, TestName = "Primitive Implicit Conversion: ulong to short")]
