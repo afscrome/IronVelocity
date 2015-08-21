12191e4334d943fda005084f3cd4cec8fd448be6
Alex Crome <afscrome@users.noreply.github.com>
2015-08-21 20:33:08 +0100

    Added some basic tests for parsing of lists

---
+++ IronVelocity.Tests/Parser/ListTests.cs
@@ -0,0 +1,23 @@
+﻿using IronVelocity.Parser;
+using NUnit.Framework;
+
+
+namespace IronVelocity.Tests.Parser
+{
+    public class ListTests : ParserTestBase
+    {
+        [TestCase("[]")]
+        [TestCase("[ ]")]
+        [TestCase("[1]")]
+        [TestCase("[ [] ]")]
+        public void ListParseTest(string input)
+        {
+            var result = CreateParser(input, VelocityLexer.ARGUMENTS).list();
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result.GetText(), Is.EqualTo(input));
+
+            Assert.That(result.arguments(), Is.Not.Null);
+        }
+    }
+}
