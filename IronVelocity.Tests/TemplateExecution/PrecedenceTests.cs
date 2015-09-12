bce14dd57d2401160ed5008a2cddc9dbb24b53e7
Alex Crome <afscrome@users.noreply.github.com>
2015-09-12 19:31:36 +0100

    Added some execution tests around operator precedence

---
+++ IronVelocity.Tests/TemplateExecution/PrecedenceTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+
+namespace IronVelocity.Tests.TemplateExecution
+{
+    public class PrecedenceTests : TemplateExeuctionBase
+    {
+        [TestCase("true || true && false", true, TestName = "when_ExecutingAnExpression_AndHasHigherPrecedenceThanOr")]
+        [TestCase("false && false == false", false, TestName = "when_ExecutingAnExpression_EqualityHasHigherPrecedenceThanAnd")]
+        [TestCase("false == true > false", true, TestName = "when_ExecutingAnExpression_RelationalHasHigherPrecedenceThanEquality")]
+        [TestCase("1 > 2 + 3", false, TestName = "when_ExecutingAnExpression_AdditiveHasHigherPrecedenceThanRelational")]
+        [TestCase("1 + 2 * 5", 11, TestName = "when_ExecutingAnExpression_MultiplicativeHasHigherPrecedenceThanAdditive")]
+
+        //The following are the same expressions as above, but using parentheses to ensure the first operator is performed first
+        //These should all give different results to above
+        [TestCase("(true || true) && false", false, TestName = "when_ExecutingAnExpression_ParenthesiedExpressionHasHigherPrecedence1")]
+        [TestCase("(false && false) == false", true, TestName = "when_ExecutingAnExpression_ParenthesiedExpressionHasHigherPrecedence2")]
+        [TestCase("(false == true) > false", false, TestName = "when_ExecutingAnExpression_ParenthesiedExpressionHasHigherPrecedence3")]
+        [TestCase("(1 > 2) + 3", null, TestName = "when_ExecutingAnExpression_ParenthesiedExpressionHasHigherPrecedence4")]
+        [TestCase("(1 + 2) * 5", 15, TestName = "when_ExecutingAnExpression_ParenthesiedExpressionHasHigherPrecedence5")]
+        public void Test(string input, object expected)
+        {
+            var result = EvaluateExpression(input);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+    }
+}
