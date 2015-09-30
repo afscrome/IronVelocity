e7ad57563b421a9b07d726ac3abc95eb6ef9dbb8
Alex Crome <afscrome@users.noreply.github.com>
2015-09-30 19:37:56 +0100

    Updated perf playground to allow switching between antlr & nvelocity to do comparisons.

--- IronVelocity.PerfPlayground/TemplateCompilation.cs
+++ IronVelocity.PerfPlayground/TemplateCompilation.cs
@@ -1,44 +1,53 @@
 ﻿using IronVelocity.Compilation;
+using IronVelocity.Compilation.AST;
+using IronVelocity.Compilation.Directives;
+using IronVelocity.Directives;
 using IronVelocity.Parser;
 using IronVelocity.Runtime;
 using NUnit.Framework;
+using NVelocity.Context;
+using NVelocity.Runtime.Directive;
+using NVelocity.Runtime.Parser.Node;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
 using System.Reflection;
 using System.Reflection.Emit;
 
 namespace IronVelocity.PerfPlayground
 {
-    [TestFixture(Category="Performance")]
+    [TestFixture(Category = "Performance")]
     [Explicit]
     public class TemplateCompilation
     {
         private const string IldasmPath = "C:\\Program Files (x86)\\Microsoft SDKs\\Windows\\v8.1A\\bin\\NETFX 4.5.1 Tools\\ildasm.exe";
 
-        public TemplateCompilation()
-        {
-            OutputDir= "CompiledTemplates";
-            TemplateDirectories = new[] {"../../templates/", "../../../IronVelocity.Tests/Regression/templates/"};
-        }
-
-        public bool SaveDlls { get; set; }
-        public bool SaveIl { get; set; }
-        public bool ExecuteTemplate { get; set; }
-        public string OutputDir { get; set; }
-        public ICollection<string> TemplateDirectories {get; set;}
+        public bool Compile { get; set; } = false;
+        public bool SaveDlls { get; set; } = false;
+        public bool SaveIl { get; set; } = false;
+        public bool ExecuteTemplate { get; set; } = false;
+        public string OutputDir { get; set; } = "CompiledTemplates";
+        public ICollection<string> TemplateDirectories { get; } = new List<string> ();
+        public string TestNamePrefix { get; set; } = "Compilation";
+        public ParserMode Mode { get; set; }
+        public ICollection<string> BlockDirectives { get; } = new List<string>();
 
         [TestFixtureSetUp]
         public void SetUp()
         {
-            OutputDir = "TemplateCompilation_" + DateTime.Now.ToString("yyy-MM-dd_HH-mm-ss") + "\\";
-            SaveDlls = false;
-            SaveIl = false;
-            ExecuteTemplate = false;
+            TemplateDirectories.Add("../../templates/");
+            TemplateDirectories.Add("../../../IronVelocity.Tests/Regression/templates/");
 
-            if (!Directory.Exists(OutputDir))
-                Directory.CreateDirectory(OutputDir);
+            if (SaveDlls || SaveIl)
+            {
+                OutputDir = TestNamePrefix + DateTime.Now.ToString("yyy-MM-dd_HH-mm-ss") + "\\";
+
+                if (!Directory.Exists(OutputDir))
+                    Directory.CreateDirectory(OutputDir);
+            }
         }
 
         [TestCaseSource("CreateTemplateTestCases")]
@@ -46,58 +55,72 @@ namespace IronVelocity.PerfPlayground
         {
             var template = File.ReadAllText(path);
             IParser parser;
-            //parser = new NVelocityParser(null, null);;
-            parser = new AntlrVelocityParser();
-            
+            switch (Mode)
+            {
+                case ParserMode.Antlr:
+                    var antlrDirectives = BlockDirectives
+                        .Select(x => new AntlrBlockDirectiveBuilder(x))
+                        .ToList<CustomDirectiveBuilder>();
+                    antlrDirectives.Add(new ForeachDirectiveBuilder());
+                    parser = new AntlrVelocityParser(antlrDirectives);
+                    break;
+                case ParserMode.NVelocity:
+                    var nvelocityDirectives = BlockDirectives.ToDictionary<string, string, DirectiveExpressionBuilder>(x => x, x => new NVelocityDirectiveExpressionBuilder(x));
+                    parser = new NVelocityParser(nvelocityDirectives, null);
+                    break;
+                default:
+                    throw new InvalidOperationException();
+            }
             var expressionTree = parser.Parse(template, assemblyName);
 
-            AssemblyBuilder assemblyBuilder = null;
-            VelocityCompiler compiler;
+            if (Compile)
+            {
+                AssemblyBuilder assemblyBuilder = null;
+                VelocityCompiler compiler;
 
-            if (SaveDlls || SaveIl)
-            {
-                var diskCompiler = new DiskCompiler(new AssemblyName(assemblyName), OutputDir);
-                assemblyBuilder = diskCompiler.AssemblyBuilder;
-                compiler = diskCompiler;
-            }
-            else
-            {
-                compiler = new VelocityCompiler(null);
-            }
-            
-            var result = compiler.CompileWithSymbols(expressionTree, assemblyName, true, path);
-
-            if (ExecuteTemplate)
-            {
-                var context = new VelocityContext();
-                using(var writer = new StringWriter())
+                if (SaveDlls || SaveIl)
                 {
-                    var output = new VelocityOutput(writer);
-                    result(context, output);
+                    var diskCompiler = new DiskCompiler(new AssemblyName(assemblyName), OutputDir);
+                    assemblyBuilder = diskCompiler.AssemblyBuilder;
+                    compiler = diskCompiler;
                 }
-            }
-
-            if (SaveDlls || SaveIl)
-            {
-                var dllName = assemblyName + ".dll";
-                assemblyBuilder.Save(dllName);
-
-                if (SaveIl)
+                else
                 {
-                    var assemblyPath = Path.Combine(OutputDir, dllName);
-                    var ilPath = assemblyPath.Replace(".dll", ".il");
-                    var startInfo = new ProcessStartInfo(IldasmPath)
+                    compiler = new VelocityCompiler(null);
+                }
+
+                var result = compiler.CompileWithSymbols(expressionTree, assemblyName, true, path);
+
+                if (ExecuteTemplate)
+                {
+                    var context = new VelocityContext();
+                    using (var writer = new StringWriter())
                     {
-                        Arguments = String.Format("\"{0}\" /item:{1} /linenum /source /out:\"{2}\"", assemblyPath, assemblyName, ilPath),
-                        CreateNoWindow = true,
-                        UseShellExecute = false,
-                        WindowStyle = ProcessWindowStyle.Hidden
-                    };
-                    Process.Start(startInfo);
+                        var output = new VelocityOutput(writer);
+                        result(context, output);
+                    }
+                }
+
+                if (SaveDlls || SaveIl)
+                {
+                    var dllName = assemblyName + ".dll";
+                    assemblyBuilder.Save(dllName);
+
+                    if (SaveIl)
+                    {
+                        var assemblyPath = Path.Combine(OutputDir, dllName);
+                        var ilPath = assemblyPath.Replace(".dll", ".il");
+                        var startInfo = new ProcessStartInfo(IldasmPath)
+                        {
+                            Arguments = String.Format("\"{0}\" /item:{1} /linenum /source /out:\"{2}\"", assemblyPath, assemblyName, ilPath),
+                            CreateNoWindow = true,
+                            UseShellExecute = false,
+                            WindowStyle = ProcessWindowStyle.Hidden
+                        };
+                        Process.Start(startInfo);
+                    }
                 }
             }
-
-
         }
 
         public IEnumerable<TestCaseData> CreateTemplateTestCases()
@@ -112,7 +135,7 @@ namespace IronVelocity.PerfPlayground
                         var relativePath = file.FullName.Replace(directory.FullName, "");
                         var assemblyName = relativePath.Replace(Path.DirectorySeparatorChar, '_').Replace(".vm", "");
                         yield return new TestCaseData(file.FullName, assemblyName)
-                           .SetName("ILGeneration: " + relativePath);
+                           .SetName(TestNamePrefix + " " + relativePath);
                     }
                 }
             }
@@ -132,7 +155,7 @@ namespace IronVelocity.PerfPlayground
                 AssemblyBuilder = AppDomain.CurrentDomain.DefineDynamicAssembly(_assemblyName, AssemblyBuilderAccess.RunAndSave, outputDir);
             }
 
-            protected override  ModuleBuilder CreateModuleBuilder(bool debugMode)
+            protected override ModuleBuilder CreateModuleBuilder(bool debugMode)
             {
                 var name = _assemblyName.Name;
                 var moduleBuilder = AssemblyBuilder.DefineDynamicModule(name, name + ".dll", true);
@@ -144,9 +167,55 @@ namespace IronVelocity.PerfPlayground
 
                 return moduleBuilder;
             }
-
-
         }
 
+        private class AntlrBlockDirectiveBuilder : CustomDirectiveBuilder
+        {
+            public AntlrBlockDirectiveBuilder(string name)
+            {
+                Name = name;
+            }
+
+            public override bool IsBlockDirective => true;
+            public override string Name { get; }
+
+            public override Expression Build(IReadOnlyList<Expression> arguments, Expression body)
+                => body;
+        }
+
+        private class NVelocityDirectiveExpressionBuilder : DirectiveExpressionBuilder
+        {
+            public NVelocityDirectiveExpressionBuilder(string name)
+            {
+                Name = name;
+            }
+
+            public override string Name { get; }
+
+            public override Type NVelocityDirectiveType => typeof(NVelocityDirective);
+
+            public override Expression Build(ASTDirective node, NVelocityNodeToExpressionConverter converter)
+            {
+                return new RenderedBlock(converter.GetBlockExpressions(node.GetChild(node.ChildrenCount - 1)));
+            }
+
+            private class NVelocityDirective : NVelocity.Runtime.Directive.Directive
+            {
+                public override string Name { get; set; } = "registerEndOfPageHtml";
+
+                public override DirectiveType Type => DirectiveType.BLOCK;
+
+                public override bool Render(IInternalContextAdapter context, TextWriter writer, INode node)
+                {
+                    throw new NotImplementedException();
+                }
+            }
+        }
+    }
+
+    public enum ParserMode
+    {
+        Antlr,
+        NVelocity
     }
 }
