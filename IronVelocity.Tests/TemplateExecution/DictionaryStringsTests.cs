26a9dd37a586a161aa40506a677c825247d4ff7d
Alex Crome <afscrome@users.noreply.github.com>
2015-10-06 22:47:50 +0100

    Migrated some legacy dictionary string related tests to the newer test format.

--- IronVelocity.Tests/TemplateExecution/DictionaryStringsTests.cs
+++ IronVelocity.Tests/TemplateExecution/DictionaryStringsTests.cs
@@ -42,7 +42,7 @@ namespace IronVelocity.Tests.TemplateExecution
         [TestCase("97", (int)97)]
         [TestCase("44.67", (float)44.67)]
         [TestCase("$x", "beautiful")]
-        public void ShouldProcessDictionaryValue(string input, object expected)
+        public void ShouldProcessDictionaryWithSIngleValueTypeValue(string input, object expected)
         {
             var dictionary = "%{ key = " + input + "}";
             var env = new
@@ -59,7 +59,7 @@ namespace IronVelocity.Tests.TemplateExecution
         }
 
         [Test]
-        public void ShouldProcessDictionaryWithSinglePrimitive()
+        public void ShouldProcessDictionaryWithSinglePrimitiveValue()
         {
             var primitive = 623;
             var input = "%{primitive = $primitive}";
@@ -85,7 +85,7 @@ namespace IronVelocity.Tests.TemplateExecution
         }
 
         [Test]
-        public void ShouldProcessDictionaryWithSingleReferenceType()
+        public void ShouldProcessDictionaryWithSingleReferenceTypeValue()
         {
             var reference = new StringBuilder();
             var input = "%{reference = $reference}";
@@ -93,8 +93,54 @@ namespace IronVelocity.Tests.TemplateExecution
 
             var result = EvaluateDictionary(input, context);
 
-            Assert.AreEqual(1, result.Count);
+            Assert.That(result, Has.Count.EqualTo(1));
             Assert.That(result["reference"], Is.EqualTo(reference));
+        }
+
+        [Test]
+        public void ShouldProcessDictionaryWithSingleInterpolatedValue()
+        {
+            var input = "%{interpolated = 32$value}";
+            var context = new { value = "fizzbuzz"};
+
+            var result = EvaluateDictionary(input, context);
+
+            Assert.That(result, Has.Count.EqualTo(1));
+            Assert.That(result["interpolated"], Is.EqualTo("32fizzbuzz"));
+        }
+
+        [Test]
+        public void ShouldProcessDictionaryStringWithNestedDictionary()
+        {
+            var input = "%{ dict = { key = 123} }";
+
+            var result = EvaluateDictionary(input);
+
+            Assert.That(result["dict"], Is.InstanceOf<IDictionary>());
+
+            var nested = (IDictionary)result["dict"];
+            Assert.That(nested, Has.Count.EqualTo(1));
+            Assert.That(nested["key"], Is.EqualTo(123));
+        }
+
+        public void ShouldProcessDictionaryStringWithEscapeChars()
+        {
+            var input = "%{ quoted = '\\'Hello\\' he said'}";
+            var result = EvaluateDictionary(input);
+
+            Assert.That(result, Has.Count.EqualTo(1));
+            Assert.That(result["quoted"], Is.EqualTo("'Hello' he said"));
+        }
+
+
+        [Test]
+        public void ShouldProcessDictionaryStringValueWithDictionaryLikeTextAsString()
+        {
+            var input = "%{ value = '{ a: b}'}";
+            var result = EvaluateDictionary(input);
+
+            Assert.That(result, Has.Count.EqualTo(1));
+            Assert.That(result["value"], Is.EqualTo("{ a: b}"));
         }
 
         [Test]
@@ -104,7 +150,7 @@ namespace IronVelocity.Tests.TemplateExecution
             var primitive = 623;
             var value = new Guid("56357676-88b0-401f-b75b-5ab124268801");
 
-            var input = "%{reference = $reference, primitive = $primitive, value = $value}";
+            var input = "%{reference = $reference, primitive = $primitive, value = $value, interpolated = 3$primitive}";
 
             var context = new
             {
@@ -117,10 +163,11 @@ namespace IronVelocity.Tests.TemplateExecution
 
             var result = EvaluateDictionary(input, context);
 
-            Assert.That(result, Has.Count.EqualTo(3));
+            Assert.That(result, Has.Count.EqualTo(4));
             Assert.That(result["reference"], Is.EqualTo(reference));
             Assert.That(result["primitive"], Is.EqualTo(primitive));
             Assert.That(result["value"], Is.EqualTo(value));
+            Assert.That(result["interpolated"], Is.EqualTo("3623"));
         }
 
         private IDictionary EvaluateDictionary(string input, object context = null)
