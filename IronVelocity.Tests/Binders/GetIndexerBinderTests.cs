ffd621bc0e429fc070e0ec53a8fe9527957d73e0
Alex Crome <afscrome@users.noreply.github.com>
2015-12-28 21:09:02 +0000

    #15 Added GetIndex binder supporting indexing on arrays

---
+++ IronVelocity.Tests/Binders/GetIndexerBinderTests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Linq;
+using IronVelocity.Binders;
+using System.Dynamic;
+using NUnit.Framework;
+
+namespace IronVelocity.Tests.Binders
+{
+    public class GetIndexerBinderTests : BinderTestBase
+    {
+        [Test]
+        public void ShouldIndexIntoSingleDimensionalPrimitiveArray()
+        {
+            var array = new int[] { 1, 2, 3 };
+
+            var result = GetIndexerTest(array, 1);
+
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void ShouldIndexIntoSingleDimensionalStructArray()
+        {
+            var array = new Guid[] {
+                new Guid("70ae656b-4826-47fb-9f23-ce3f8b85dfb0"),
+                new Guid("5b001171-8db6-47db-9830-0bab734c7d19"),
+                new Guid("af90933f-6e89-4c50-9136-3bd422cd7daa"),
+                new Guid("0eb4bc26-e77b-444e-84db-2239a936a771")
+            };
+
+            var result = GetIndexerTest(array, 2);
+
+            Assert.That(result, Is.EqualTo(new Guid("af90933f-6e89-4c50-9136-3bd422cd7daa")));
+        }
+
+        [Test]
+        public void ShouldIndexIntoSingleDimensionalReferenceTypeArray()
+        {
+            var array = new object[] {
+                "foo",
+                123
+            };
+
+            var result = GetIndexerTest(array, 0);
+
+            Assert.That(result, Is.EqualTo("foo"));
+        }
+
+        [Test]
+        public void ShouldIndexIntoTwoDimensionalArray()
+        {
+            var array = new int[,] {
+                {1,2,3,4},
+                {5,6,7, 8},
+                {9,10,11,12}
+            };
+
+            var result = GetIndexerTest(array, 1, 3);
+
+            Assert.That(result, Is.EqualTo(8));
+        }
+        [Test]
+        public void ShouldFailToIndexArrayWithNonIntegerArguments()
+        {
+            var array = new double[] { 3.14, 8.92 };
+
+            var result = GetIndexerTest(array, "fizz");
+
+            Assert.That(result, Is.Null);
+        }
+
+
+        private object GetIndexerTest(object input, params object[] args)
+        {
+            var binder = new VelocityGetIndexBinder(new CallInfo(args.Length));
+            args = new[] { input }.Concat(args).ToArray();
+
+            return InvokeBinder(binder, args);
+        }
+    }
+}
---
+++ IronVelocity/Binders/VelocityGetIndexBinder.cs
@@ -0,0 +1,75 @@
+﻿using IronVelocity.Compilation;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IronVelocity.Binders
+{
+    public class VelocityGetIndexBinder : GetIndexBinder
+    {
+        public VelocityGetIndexBinder(CallInfo callInfo):
+            base(callInfo)
+        {
+
+        }
+
+        public override DynamicMetaObject FallbackGetIndex(DynamicMetaObject target, DynamicMetaObject[] indexes, DynamicMetaObject errorSuggestion)
+        {
+            if (target == null)
+                throw new ArgumentNullException(nameof(target));
+            if (indexes == null)
+                throw new ArgumentNullException(nameof(indexes));
+
+            if (!target.HasValue || indexes.Any(x => !x.HasValue))
+                return Defer(target, indexes);
+
+            // If the target has a null value, then we won't be able to get any fields or properties, so escape early
+            // Failure to escape early like this results in an infinite loop
+            if (target.Value == null)
+                return BinderHelper.NullTargetResult(target,errorSuggestion);
+
+            var arrayRank = target.LimitType.GetArrayRank();
+
+            return arrayRank == 0
+                ? CustomIndexer(target, indexes, errorSuggestion)
+                : ArrayIndexer(arrayRank, target, indexes, errorSuggestion);
+        }
+
+        protected virtual DynamicMetaObject ArrayIndexer(int arrayRank, DynamicMetaObject target, DynamicMetaObject[] indexes, DynamicMetaObject errorSuggestion)
+        {
+            Expression result;
+            var restrictions = BinderHelper.CreateCommonRestrictions(target, indexes);
+            if (arrayRank == indexes.Length && indexes.All(x => x.LimitType == typeof(int)))
+            {
+                result = VelocityExpressions.ConvertIfNeeded(Expression.ArrayAccess(target.Expression, indexes.Select(x => x.Expression).ToArray()), ReturnType);
+
+                return new DynamicMetaObject(result, restrictions);
+            }
+            else
+            {
+                if (errorSuggestion != null)
+                {
+                    result= errorSuggestion.Expression;
+                    restrictions = restrictions.Merge(errorSuggestion.Restrictions);
+                }
+                else
+                {
+                    result = Constants.VelocityUnresolvableResult;
+                }
+            }
+
+            return new DynamicMetaObject(result, restrictions);
+        }
+
+
+        protected virtual DynamicMetaObject CustomIndexer(DynamicMetaObject target, DynamicMetaObject[] indexes, DynamicMetaObject errorSuggestion)
+        {
+            throw new NotImplementedException();
+
+        }
+    }
+}
