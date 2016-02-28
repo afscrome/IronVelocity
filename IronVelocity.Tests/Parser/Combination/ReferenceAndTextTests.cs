aad0469ac0a00b88aabbc190b8214c6c8100e9cf
Alex Crome <afscrome@users.noreply.github.com>
2016-02-28 12:59:38 +0000

    Refactored Combination Tests

---
+++ IronVelocity.Tests/Parser/Combination/ReferenceAndTextTests.cs
@@ -0,0 +1,85 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IronVelocity.Tests.Parser.Combination
+{
+    public class ReferenceAndTextTests : ParserTestBase
+    {
+        [TestCase("$test.stuff()($test", "$test.stuff()", "(", "$test")]
+        [TestCaseSource(nameof(TwoReferencesWithPotentiallyAmbigiousCharcterInBetween))]
+        public void ParseTwoReferenceswithTextInBetween(string input, string reference1, string text, string reference2)
+        {
+            var result = Parse(input, x => x.block());
+            var textNode = result.text().Single();
+            Assert.That(textNode.GetText(), Is.EqualTo(text));
+
+            var referenceTexts = result.reference().Select(x => x.GetText());
+
+            Assert.That(referenceTexts, Contains.Item(reference1));
+            Assert.That(referenceTexts, Contains.Item(reference2));
+        }
+
+        [TestCaseSource(nameof(CombinationTestSource))]
+        public void CombinationTest(string input, Type expectedLeftNodeType, Type expectedRightNodeType, string leftExpectedText, string rightExpectedText)
+        {
+            var result = Parse(input, x => x.block());
+            Assert.That(result.ChildCount, Is.EqualTo(2));
+
+            var left = result.GetChild(0);
+            var right = result.GetChild(1);
+
+            Assert.That(left, Is.TypeOf(expectedLeftNodeType));
+            Assert.That(right, Is.TypeOf(expectedRightNodeType));
+
+            Assert.That(left.GetText(), Is.EqualTo(leftExpectedText));
+            Assert.That(right.GetText(), Is.EqualTo(rightExpectedText));
+        }
+
+
+        public IEnumerable<TestCaseData> TwoReferencesWithPotentiallyAmbigiousCharcterInBetween()
+        {
+            foreach (var text in Samples.GetSamples(SampleType.AmbigiousDelimiters))
+            {
+                foreach (var left in Samples.GetSamples(SampleType.Reference))
+                {
+                    foreach (var right in Samples.GetSamples(SampleType.Reference))
+                    {
+                        var input = left + text + right;
+                        yield return new TestCaseData(input, left, text, right)
+                            .SetName("ParseTwoReferencesWithTextInBetween - " + input);
+                    }
+                }
+            }
+        }
+
+        public IEnumerable<TestCaseData> CombinationTestSource()
+        {
+            return CreateCombinationTestsWithTwoNodeResult(SampleType.Reference, SampleType.AmbigiousDelimiters)
+                .Concat(CreateCombinationTestsWithTwoNodeResult(SampleType.Reference, SampleType.ReferenceLikeText))
+                .Concat(CreateCombinationTestsWithTwoNodeResult(SampleType.ReferenceLikeText, SampleType.Reference))
+                .Concat(CreateCombinationTestsWithTwoNodeResult(SampleType.AmbigiousDelimiters, SampleType.Reference))
+                .Concat(CreateCombinationTestsWithTwoNodeResult(SampleType.Reference, SampleType.Reference));
+        }
+
+        public IEnumerable<TestCaseData> CreateCombinationTestsWithTwoNodeResult(SampleType leftType, SampleType rightType)
+        {
+            var leftNodeType = Samples.GetSampleParserNodeType(leftType);
+            var rightNodeType = Samples.GetSampleParserNodeType(rightType);
+
+            foreach (var left in Samples.GetSamples(leftType))
+            {
+                foreach (var right in Samples.GetSamples(rightType))
+                {
+                    if (left.EndsWith("#") && (right.StartsWith("#") || right.StartsWith("{")))
+                        continue;
+
+                    var input = left + right;
+                    yield return new TestCaseData(input, leftNodeType, rightNodeType, left, right)
+                        .SetName($"zCombination Test - {leftType}, {rightType} - {input}");
+                }
+            }
+        }
+    }
+}
--- IronVelocity.Tests/Parser/Samples.cs
+++ IronVelocity.Tests/Parser/Samples.cs
@@ -1,10 +1,54 @@
-﻿using System.Collections.Generic;
+﻿using IronVelocity.Parser;
+using System;
+using System.Collections.Generic;
 
 namespace IronVelocity.Tests.Parser
 {
     public static class Samples
     {
-        public static IEnumerable<string> References { get; } = new[]
+        public static IReadOnlyCollection<string> GetSamples(SampleType type)
+        {
+            switch (type)
+            {
+                case SampleType.ReferenceLikeText:
+                    return ReferenceLikeText;
+                case SampleType.AmbigiousDelimiters:
+                    return AmbigiousDelimiters;
+                case SampleType.Reference:
+                    return References;
+                case SampleType.BlockComment:
+                    return BlockComments;
+                case SampleType.LineComment:
+                    return SingleLineComments;
+              default:
+                    throw new ArgumentOutOfRangeException(nameof(type));
+            }
+
+        }
+
+        public static Type GetSampleParserNodeType(SampleType type)
+        {
+            switch (type)
+            {
+                case SampleType.ReferenceLikeText:
+                case SampleType.AmbigiousDelimiters:
+                    return typeof(VelocityParser.TextContext);
+                case SampleType.Reference:
+                    return typeof(VelocityParser.ReferenceContext);
+                case SampleType.BlockComment:
+                case SampleType.LineComment:
+                    return typeof(VelocityParser.CommentContext);
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(type));
+            }
+        }
+
+        public static bool MayBeProblematicToCombine(string left, string right)
+        {
+            return left.EndsWith("#") && (right.StartsWith("#") || right.StartsWith("{"));
+        }
+
+        private static string[] References { get; } = new[]
         {
             "$informal", "$!silent", "${formal}", "$!{formalsilent}",
             "$informal.property", "$!silent.property", "${formal.property}", "$!{formalsilent.property}",
@@ -15,7 +59,7 @@ namespace IronVelocity.Tests.Parser
         /// Some examples of text that starts like a reference, but isn't as there is no Identifier.
         /// These patterns have historically caused lots of problems 
         /// </summary>
-        public static IEnumerable<string> ReferenceLikeText { get; } = new[]
+        private static string[] ReferenceLikeText { get; } = new[]
         {
             "$",
             "$123",
@@ -38,7 +82,7 @@ namespace IronVelocity.Tests.Parser
             "${}",
         };
 
-        public static IEnumerable<string> PotentiallyAmbigiousReferenceDelimiters { get; } = new[]
+        private static string[] AmbigiousDelimiters { get; } = new[]
         {
             ".",
             ".(",
@@ -54,18 +98,28 @@ namespace IronVelocity.Tests.Parser
             "#}",
         };
 
-        public static IEnumerable<string> Comments { get; } = new[]
+        private static string[] SingleLineComments { get; } = new[]
         {
-            //Single line test are causing problems due to the newline at the end becoming text.
-            /*
             "##Single\r",
             "##Line\n",
             "##Comment\r\n",
-            */
+        };
+
+        private static string[] BlockComments { get; } = new[]
+        {
             "#*SingleLineBlock*#",
             "#*Multi\rLine\nBlock*#",
             "#*Nested #*Block*# #*Comment*# *#"
         };
 
+    }
+
+    public enum SampleType
+    {
+        ReferenceLikeText,
+        AmbigiousDelimiters,
+        Reference,
+        BlockComment,
+        LineComment
     }
 }
