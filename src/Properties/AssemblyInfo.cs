c0b34e310f91f1129ba7a33d03fe4922dd721910
Alex Crome <afscrome@users.noreply.github.com>
2018-12-22 23:04:01 +0000

    Moved to .Net Sdk (#76)
    * Migrated projects to new .Net SDK Format
    * Restructured solution directory folder
    * Removed no longer used `IronVelocity.Scripting` project
    * Upgraded NUnit to be compatible with dotnet test
    * Upgraded Antlr to be compatible with dotnet build
    * Added Azure Pipelines to build for dotnet support

---
+++ src/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(false)]
+[assembly: InternalsVisibleTo("IronVelocity.Tests")]
---
+++ test/IronVelocity/Parser/TextTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace IronVelocity.Tests.Parser
+{
+    public class TextTests : ParserTestBase
+    {
+        [TestCase("Hello World")]
+        [TestCaseSource(nameof(TextTests.TextThatLooksLikeReferenceData))]
+        public void ParseText(string input)
+        {
+            var text = Parse(input, x => x.text());
+            Assert.That(text.GetText(), Is.EqualTo(input));
+        }
+
+
+        public static IEnumerable<TestCaseData> TextThatLooksLikeReferenceData()
+            => Samples.GetSamples(SampleType.ReferenceLikeText)
+                .Select(x => new TestCaseData(x).SetName("ParseTextThatLooksLikeReference - " + x));
+    }
+}
