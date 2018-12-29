b3c9acc6f4497eda736e0abdd9c0fe35ade1a04b
Alex Crome <afscrome@users.noreply.github.com>
2018-12-29 13:08:05 +0000

    Added SourceText and LineText
    These hold the original text, and can be used to convert position data into line data.

---
+++ src/CodeAnalysis/Text/TextLine.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+
+namespace IronVelocity.CodeAnalysis.Text
+{
+    [DebuggerDisplay("{Start}:{Length}({LengthIncludingLineBreak}) {Text}")]
+    public class TextLine
+    {
+        public TextLine(int lineNumber, int start, int length, int lineBreakLength)
+        {
+            LineNumber = lineNumber;
+            Start = start;
+            Length = length;
+            LengthIncludingLineBreak = lineBreakLength;
+        }
+
+        public int LineNumber { get; }
+        public int Start { get; }
+        public int Length { get; }
+        public int LengthIncludingLineBreak { get; }
+        public int End => Start + Length;
+    }
+}
