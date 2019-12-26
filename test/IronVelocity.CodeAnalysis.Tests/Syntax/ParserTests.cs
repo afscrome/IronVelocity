4196c45c46d6e772c1353102892ca2836e27d731
Alex Crome <afscrome@users.noreply.github.com>
2019-12-26 00:05:48 +0000

    Improved Parser Tests
    Introduced a light weight DSL for asserting parse tree structure.
    Expanded parser tests using this DSL

--- test/IronVelocity.CodeAnalysis.Tests/Syntax/ParserTests.cs
+++ test/IronVelocity.CodeAnalysis.Tests/Syntax/ParserTests.cs
@@ -1,60 +1,107 @@
 using IronVelocity.CodeAnalysis.Syntax;
 using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using static IronVelocity.Tests.CodeAnalysis.Syntax.SyntaxTreeAssertions;
 
 namespace IronVelocity.Tests.CodeAnalysis.Syntax
 {
-    public class ParserTests
+    public partial class ParserTests
     {
         [Test]
-        public void ParsesNumber()
+        public void Parses_NumberLiteral()
         {
-            var tokens = new SyntaxToken[] {
-                Number(1234)
-            };
-
-            var parser = new Parser(tokens);
-
-            var expression = parser.Parse()?.Root;
-
-            Assert.That(expression, Is.InstanceOf<LiteralExpressionSyntax>());
-            var literal = (LiteralExpressionSyntax)expression!;
-            Assert.That(literal.Value, Is.EqualTo(1234));
-            Assert.That(literal.LiteralToken, Is.EqualTo(tokens[0]));
+            AssertParseTree("1234", Literal(1234));
         }
 
-        [Test]
-        public void ParsesBasicBinaryExpression()
+        [TestCase("true", true)]
+        [TestCase("false", false)]
+        public void Parses_BooleanLiteral(string text, object expectedValue)
         {
-            var tokens = new SyntaxToken[]
+            AssertParseTree(text, Literal(expectedValue));
+        }
+
+        [TestCaseSource(typeof(SyntaxFacts), nameof(SyntaxFacts.GetUnaryOperatorKinds))]
+        public void Parses_AllUnaryExpressionOperators(SyntaxKind kind)
+        {
+            var text = $"{SyntaxFacts.GetText(kind)}0";
+
+            AssertParseTree(text,
+                Unary(
+                    kind,
+                    Literal(0)
+                )
+            );
+        }
+
+        [TestCaseSource(typeof(SyntaxFacts), nameof(SyntaxFacts.GetBinaryOperatorKinds))]
+        public void Parses_AllBinaryExpressionOperators(SyntaxKind kind)
+        {
+            var text = $"true {SyntaxFacts.GetText(kind)} false";
+
+            AssertParseTree(text,
+                Binary(
+                    Literal(true),
+                    kind,
+                    Literal(false)
+                )
+            );
+        }
+
+        [TestCaseSource(nameof(BinaryOperatorPairs))]
+        public void Parses_MultipleBinaryExpression_HonouringPrecedence(SyntaxKind left, SyntaxKind right)
+        {
+            var leftPrecedence = SyntaxFacts.GetBinaryOperatorPrecedence(left);
+            var rightPrecedence = SyntaxFacts.GetBinaryOperatorPrecedence(right);
+
+            var leftText = SyntaxFacts.GetText(left);
+            var rightText = SyntaxFacts.GetText(right);
+
+            var text = $"1 {leftText} 2 {rightText} 3";
+
+            if (rightPrecedence > leftPrecedence)
             {
-                Number(45),
-                new SyntaxToken(SyntaxKind.PlusToken, 0, "+"),
-                Number(72),
-            };
+                AssertParseTree(text,
+                    Binary(
+                        Literal(1),
+                        left,
+                        Binary(
+                            Literal(2),
+                            right,
+                            Literal(3)
+                        )
 
-            var parser = new Parser(tokens);
-
-            var expression = parser.Parse()?.Root;
-
-            Assert.That(expression, Is.InstanceOf<BinaryExpressionSyntax>());
-
-            var binaryExpression = (BinaryExpressionSyntax)expression!;
-            Assert.That(binaryExpression.Left, Is.InstanceOf<LiteralExpressionSyntax>());
-            Assert.That(binaryExpression.Right, Is.InstanceOf<LiteralExpressionSyntax>());
-
-            var left = (LiteralExpressionSyntax)binaryExpression.Left;
-            var right = (LiteralExpressionSyntax)binaryExpression.Right;
-
-            Assert.That(left.LiteralToken, Is.EqualTo(tokens[0]));
-            Assert.That(binaryExpression.OperatorToken, Is.EqualTo(tokens[1]));
-            Assert.That(right.LiteralToken, Is.EqualTo(tokens[2]));
+                    )
+                );
+            }
+            else
+            {
+                AssertParseTree(text,
+                    Binary(
+                        Binary(
+                            Literal(1),
+                            left,
+                            Literal(2)
+                        ),
+                        right,
+                        Literal(3)
+                    )
+                );
+            }
         }
 
 
+        private static IEnumerable<object[]> BinaryOperatorPairs()
+            => GetAllPairs(SyntaxFacts.GetBinaryOperatorKinds());
+        private static IEnumerable<object[]> UnaryOperatorPairs()
+            => GetAllPairs(SyntaxFacts.GetUnaryOperatorKinds());
 
-
-        public SyntaxToken Number(int value)
-            => new SyntaxToken(SyntaxKind.NumberToken, 0, value.ToString(), value);
+        public static IEnumerable<object[]> GetAllPairs<T>(IEnumerable<T> values)
+        {
+            return from left in values
+                   from right in values
+                   select new object[] { left, right };
+        }
 
     }
 }
---
+++ test/IronVelocity.CodeAnalysis.Tests/Syntax/SyntaxTreeAssertions.cs
@@ -0,0 +1,125 @@
+﻿using IronVelocity.CodeAnalysis.Syntax;
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using System.Linq;
+
+namespace IronVelocity.Tests.CodeAnalysis.Syntax
+{
+    public class SyntaxTreeAssertions
+    {
+        public static void AssertParseTree(string text, SyntaxTreeAssertions assertions)
+        {
+            var tokens = new Lexer(text).ReadAllTokens();
+            var parser = new Parser(tokens);
+            var syntaxTree = parser.Parse();
+
+            AssertParseTree(syntaxTree, assertions);
+        }
+
+        public static void AssertParseTree(SyntaxTree syntaxTree, SyntaxTreeAssertions assertions)
+        {
+            Assert.That(syntaxTree.Diagnostics, Is.Empty);
+            assertions.AssertNode(syntaxTree.Root);
+        }
+
+        public static SyntaxTreeAssertions Literal(object value)
+        {
+            return new SyntaxTreeAssertions()
+                .AssertKind(SyntaxKind.LiteralExpression)
+                .AssertType<LiteralExpressionSyntax>()
+                .AddAssertion<LiteralExpressionSyntax>(x => Assert.That(x.Value, Is.EqualTo(value)));
+        }
+
+        public static SyntaxTreeAssertions Binary(SyntaxTreeAssertions left, SyntaxKind op, SyntaxTreeAssertions right)
+        {
+            return new SyntaxTreeAssertions()
+                .AssertKind(SyntaxKind.BinaryExpression)
+                .AssertType<BinaryExpressionSyntax>()
+                .WithChildAssertions(
+                    left,
+                    new SyntaxTreeAssertions()
+                        .AssertKind(op)
+                        .AssertType<SyntaxToken>(),
+                    right
+                );
+        }
+
+        public static SyntaxTreeAssertions Unary(SyntaxKind op, SyntaxTreeAssertions operand)
+        {
+            return new SyntaxTreeAssertions()
+                .AssertKind(SyntaxKind.UnaryExpression)
+                .AssertType<UnaryExpressionSyntax>()
+                .WithChildAssertions(
+                    new SyntaxTreeAssertions()
+                        .AssertKind(op)
+                        .AssertType<SyntaxToken>(),
+                    operand
+                );
+        }
+
+        private readonly List<Action<SyntaxNode>> _assertions = new List<Action<SyntaxNode>>();
+        private readonly List<SyntaxTreeAssertions> _childAssertions = new List<SyntaxTreeAssertions>();
+
+
+        private SyntaxTreeAssertions AssertType<T>()
+            => AddAssertion(x => Assert.That(x, Is.TypeOf<T>()));
+
+        private SyntaxTreeAssertions AssertKind(SyntaxKind kind)
+            => AddAssertion(x => Assert.That(x.Kind, Is.EqualTo(kind)));
+
+        private SyntaxTreeAssertions AddAssertion<T>(Action<T> assertion)
+            where T : SyntaxNode
+        {
+            return AssertType<T>().
+                AddAssertion(x =>
+                {
+                    if (x is T castNode)
+                    {
+                        assertion(castNode);
+                    }
+                });
+        }
+
+        private SyntaxTreeAssertions AddAssertion(Action<SyntaxNode> assertion)
+        {
+            _assertions.Add(assertion);
+            return this;
+        }
+
+        public SyntaxTreeAssertions WithChildAssertions(params SyntaxTreeAssertions[] childAssertions)
+        {
+            _childAssertions.Clear();
+            _childAssertions.AddRange(childAssertions);
+            return this;
+        }
+
+        public void AssertNode(SyntaxNode node)
+        {
+            foreach(var assetion in _assertions)
+            {
+                assetion(node);
+            }
+
+            if (!_childAssertions.Any())
+            {
+                return;
+            }
+
+            var childNodes = node.GetChildren();
+            if (childNodes.Length == _childAssertions.Count)
+            {
+                for (int i = 0; i < childNodes.Length; i++)
+                {
+                    _childAssertions[i].AssertNode(childNodes[i]);
+                }
+            }
+            else
+            {
+                Assert.That(_childAssertions.Count, Is.EqualTo(childNodes.Length));
+            }
+        }
+
+
+    }
+}
